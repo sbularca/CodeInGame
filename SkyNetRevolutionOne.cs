c4c148e459beaf881f3d06cd52f97fdd71b14e20
Sebastian Bularca <sebastian@tocaboca.com>
2018-07-04 17:19:26 +0200

    wip

--- SkyNetRevolutionOne.cs
+++ SkyNetRevolutionOne.cs
@@ -1,93 +1,96 @@
 using System;
-using System.Linq;
-using System.IO;
-using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
-namespace CodeInGame {
+namespace CodeInGame
+{
 
-    public class SkyNet {
+    public class SkyNet
+    {
 
-        public static void SkyNetRevolutionOne () {
+        public static void SkyNetRevolutionOne ()
+        {
 
-            string [] inputs;
-            inputs = Console.ReadLine().Split(' ');
-            int N = int.Parse(inputs [0]); // the total number of nodes in the level, including the gateways
-            int L = int.Parse(inputs [1]); // the number of links
-            int E = int.Parse(inputs [2]); // the number of exit gateways
-
-            Node<bool> [] vertexes = new Node<bool>[N];
-            for (int i = 0; i<N; i++)
-            {
-                vertexes[i] = new Node<bool>();
-            }
-
-            for (int i = 0 ; i < L ; i++) {
+                string [] inputs;
                 inputs = Console.ReadLine().Split(' ');
-                int N1 = int.Parse(inputs [0]); // N1 and N2 defines a link between these nodes
-                int N2 = int.Parse(inputs [1]);
-                vertexes[N1].links.Add(N2);
-            }
+                int N = int.Parse(inputs [0]); // the total number of nodes in the level, including the gateways
+                int L = int.Parse(inputs [1]); // the number of links
+                int E = int.Parse(inputs [2]); // the number of exit gateways
 
-            for (int i = 0 ; i < E ; i++) {
-                int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
-                vertexes[EI].data = true;
-            }
-
-            // game loop
-            while (true) {
-                int agentIndex = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
-
-                if (vertexes[agentIndex].links.Count > 0)
+                Node<int> [] nodes = new Node<int> [N];
+                for (int i = 0 ; i < N ; i++)
                 {
-                    string linkToCut = SearchTree(vertexes.ToList(), vertexes[agentIndex], agentIndex);
-                    
-                    if (linkToCut!=null)
-                        Console.WriteLine(linkToCut);
+                    nodes [i] = new Node<int>();
+                    nodes[i].data = i;
                 }
-                
-            }
 
-            Console.WriteLine("\nPress Ctrl+X to go back to menu.");
-            ConsoleKeyInfo cki = Console.ReadKey();
-            if ((cki.Modifiers & ConsoleModifiers.Control) != 0 && cki.Key == ConsoleKey.X) {
-                MainMenu.Main(null);
-            }
+                for (int i = 0 ; i < L ; i++)
+                {
+                    inputs = Console.ReadLine().Split(' ');
+                    int N1 = int.Parse(inputs [0]); // N1 and N2 defines a link between these nodes
+                    int N2 = int.Parse(inputs [1]);
+                    nodes[N1].links.Add(N2);
+                    nodes[N2].links.Add(N2);
+                }
 
-            SkyNetRevolutionOne();
-        }
+                for (int i = 0 ; i < E ; i++)
+                {
+                    int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
+                    nodes[EI].hasData = true;
+                    Console.Error.WriteLine(EI);
+                }
 
-        private static string SearchTree (List<Node<bool>> vertexes, Node<bool> node, int agentIndex) {
+                //Console.WriteLine("\nPress Ctrl+X to go back to menu.");
 
-            string linkToCut = null;
-            List<Node<bool>> nextNodesSet = new List<Node<bool>>();
-            nextNodesSet.Add(node);
+                // game loop
+                while (true)
+                {
+                    int agentIndex = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
 
-            while (nextNodesSet.Count > 0) {
-                for (int i = 0 ; i < nextNodesSet.Count ; i++) {
+                    if (nodes[agentIndex].links.Count > 0)
+                    {
+                        string linkToCut = SearchTree(nodes.ToList(), agentIndex);
 
-                    if (nextNodesSet [i].links.Count > 0) {
-                        for (int j = 0 ; j < nextNodesSet [i].links.Count ; j++) {
-
-                            int nodeIndex = nextNodesSet [i].links [j];
-
-                            if (HasGateway(nextNodesSet [j])) {
-                                linkToCut = agentIndex +" " + nodeIndex;
-                                return linkToCut;
-                            }
-                            agentIndex = nodeIndex;
-                            nextNodesSet.Add(vertexes [nodeIndex]);
-                            nextNodesSet.Remove(nextNodesSet [i]);
-                        }
+                        if (linkToCut != null)
+                            Console.WriteLine(linkToCut);
                     }
-                }
-            }
-            return null;
+
+                    /*ConsoleKeyInfo cki = Console.ReadKey();
+                    if ((cki.Modifiers & ConsoleModifiers.Control) != 0 && cki.Key == ConsoleKey.X)
+                    {
+                        MainMenu.Main(null);
+                    } else
+                    SkyNetRevolutionOne();  */  
+                }           
         }
 
-        private static bool HasGateway(Node<bool> node)
-        {   
+        private static string SearchTree (List<Node<bool>> nodesList, int agentIndex)
+        {
+            string linkToCut = null;
+            int nodeToCheckIndex = agentIndex;
+            int gatewayIndex = 0;
+            Console.Error.WriteLine(nodesList[agentIndex].links.Count);
+
+            for (int i = 0 ; i < nodesList[agentIndex].links.Count ; i++)
+            {   
+
+                nodeToCheckIndex = nodesList[agentIndex].links[i];
+                linkToCut = nodeToCheckIndex + " " + agentIndex;
+                
+                if (HasGateway(nodesList[nodeToCheckIndex]))
+                {
+                    gatewayIndex = nodeToCheckIndex;
+                    linkToCut = gatewayIndex + " " + agentIndex;
+                }
+            }
+            return linkToCut;
+        }
+
+        private static bool HasGateway (Node<bool> node)
+        {
             if (node.data)
                 return true;
             return false;
@@ -95,8 +98,10 @@ namespace CodeInGame {
 
     }
 
-    public class Node <T> {
+    public class Node<T>
+    {
         public T data;
+        public bool hasData;
         public List<int> links;
     }
 
