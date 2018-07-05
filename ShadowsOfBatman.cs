b5aaf00ed5888ed6aace3d746342a745635aa3ed
Sebastian Bularca <sebastian@tocaboca.com>
2018-07-05 15:58:30 +0200

    wip shadows

--- ShadowsOfBatman.cs
+++ ShadowsOfBatman.cs
@@ -4,34 +4,88 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Net.Http.Headers;
+using System.Reflection;
 
 namespace CodeInGame
 {
     public class ShadowsOfBatman
     {
-        public static void ShadowsOfBatmanOne ()
+        public static void ShadowsOfBatmanOne()
         {
             string[] inputs;
+
             inputs = Console.ReadLine().Split(' ');
             int W = int.Parse(inputs[0]); // width of the building.
             int H = int.Parse(inputs[1]); // height of the building.
             int N = int.Parse(Console.ReadLine()); // maximum number of turns before game over.
+
             inputs = Console.ReadLine().Split(' ');
-            int X0 = int.Parse(inputs[0]);
-            int Y0 = int.Parse(inputs[1]);
+            int Y0 = int.Parse(inputs[0]);
+            int X0 = int.Parse(inputs[1]);
+
+            int[,][] board = new int[H, W][];
+            int[] currentPosition = new int[2];
+            int[] previusPosition = new int[2];
+
+            currentPosition[0] = X0;
+            currentPosition[1] = Y0;
+
+            for (int h = 0; h < H; H++)
+            {
+                for (int w = 0; w < W; w++)
+                {
+                    board[h, w] = new int[2];
+                    board[h, w][0] = h;
+                    board[h, w][1] = w;
+                }
+            }
+
 
             // game loop
             while (true)
             {
                 string bombDir = Console.ReadLine(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+                int height = 0;
+                int width = 0;
+
+                string result = "0 0";
+                switch (bombDir)
+                {
+                    case "U":
+                        height = Math.Round(currentPosition[0] / 2) as decimal;
+                        break;
+                    case "UR":
+                        break;
+                    case "R":
+                        break;
+                    case "DR":
+                        break;
+                    case "D":
+                        break;
+                    case "DL":
+                        break;
+                    case "L":
+                        break;
+                    case "UL":
+                        break;
+                }
+
+
+                previusPosition[0] = currentPosition[0];
+                previusPosition[1] = currentPosition[1];
 
                 // Write an action using Console.WriteLine()
                 // To debug: Console.Error.WriteLine("Debug messages...");
 
 
                 // the location of the next window Batman should jump to.
-                Console.WriteLine("0 0");
+                Console.WriteLine(result);
             }
+        }
+
+        public static string GetCoordinates(int H, int W){
+            return null;
         }
     }
 }
--- SkyNetRevolutionOne.cs
+++ SkyNetRevolutionOne.cs
@@ -11,36 +11,36 @@ namespace CodeInGame
     public class SkyNet
     {
 
-        public static void SkyNetRevolutionOne ()
+        public static void SkyNetRevolutionOne()
         {
-                string [] inputs;
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+            int N = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
+            int L = int.Parse(inputs[1]); // the number of links
+            int E = int.Parse(inputs[2]); // the number of exit gateways
+
+            Node<int>[] nodes = new Node<int>[N];
+            for (int i = 0; i < N; i++)
+            {
+                nodes[i] = new Node<int>();
+                nodes[i].data = i;
+            }
+
+            for (int i = 0; i < L; i++)
+            {
                 inputs = Console.ReadLine().Split(' ');
-                int N = int.Parse(inputs [0]); // the total number of nodes in the level, including the gateways
-                int L = int.Parse(inputs [1]); // the number of links
-                int E = int.Parse(inputs [2]); // the number of exit gateways
+                int N1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
+                int N2 = int.Parse(inputs[1]);
+                nodes[N1].links.Add(N2);
+                nodes[N2].links.Add(N1);
+            }
 
-                Node<int> [] nodes = new Node<int> [N];
-                for (int i = 0 ; i < N ; i++)
-                {
-                    nodes [i] = new Node<int>();
-                    nodes[i].data = i;
-                }
-
-                for (int i = 0 ; i < L ; i++)
-                {
-                    inputs = Console.ReadLine().Split(' ');
-                    int N1 = int.Parse(inputs [0]); // N1 and N2 defines a link between these nodes
-                    int N2 = int.Parse(inputs [1]);
-                    nodes[N1].links.Add(N2);
-                    nodes[N2].links.Add(N1);
-                }
-
-                for (int i = 0 ; i < E ; i++)
-                {
-                    int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
-                    nodes.FirstOrDefault(x => x.data == EI).hasData = true;
-                    Console.Error.WriteLine(EI);
-                }
+            for (int i = 0; i < E; i++)
+            {
+                int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
+                nodes.FirstOrDefault(x => x.data == EI).hasData = true;
+                Console.Error.WriteLine(EI);
+            }
 
             Console.WriteLine("\nPress Ctrl+X to go back to menu.");
 
@@ -50,20 +50,20 @@ namespace CodeInGame
                 int agentNode = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
                 int indexOfAgentNode = Array.IndexOf(nodes, nodes.FirstOrDefault(x => x.data == agentNode));
 
-                if (nodes [indexOfAgentNode].links.Count > 0)
+                if (nodes[indexOfAgentNode].links.Count > 0)
                 {
                     string linkToCut = null;
 
-                    int valueOfAgentNode = nodes [indexOfAgentNode].data;
+                    int valueOfAgentNode = nodes[indexOfAgentNode].data;
 
                     Console.Error.WriteLine("Agent node index is:" + indexOfAgentNode);
 
-                    for (int i = 0 ; i < nodes [indexOfAgentNode].links.Count ; i++)
+                    for (int i = 0; i < nodes[indexOfAgentNode].links.Count; i++)
                     {
-                        int valueOfNodeToCheck = nodes [indexOfAgentNode].links [i];
+                        int valueOfNodeToCheck = nodes[indexOfAgentNode].links[i];
                         linkToCut = valueOfNodeToCheck + " " + valueOfAgentNode;
 
-                         Console.Error.WriteLine("Node checked is: " + valueOfNodeToCheck);
+                        Console.Error.WriteLine("Node checked is: " + valueOfNodeToCheck);
                         if (nodes.FirstOrDefault(x => x.data == valueOfNodeToCheck).hasData)
                         {
                             Console.Error.WriteLine("Gateway is:" + valueOfNodeToCheck);
@@ -78,9 +78,10 @@ namespace CodeInGame
                 if ((cki.Modifiers & ConsoleModifiers.Control) != 0 && cki.Key == ConsoleKey.X)
                 {
                     MainMenu.Main(null);
-                } else
-                SkyNetRevolutionOne();
-            }           
+                }
+                else
+                    SkyNetRevolutionOne();
+            }
         }
     }
 
