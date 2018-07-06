fe8b49506ba371215b402eff18bcbcde0158502f
Sebastian Bularca <sebastian@tocaboca.com>
2018-07-06 15:39:24 +0200

    shadows of batman wip

--- ShadowsOfBatman.cs
+++ ShadowsOfBatman.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Net.Http.Headers;
 using System.Reflection;
+using System.Transactions;
 
 namespace CodeInGame
 {
@@ -21,59 +22,125 @@ namespace CodeInGame
             int N = int.Parse(Console.ReadLine()); // maximum number of turns before game over.
 
             inputs = Console.ReadLine().Split(' ');
-            int Y0 = int.Parse(inputs[0]);
-            int X0 = int.Parse(inputs[1]);
+            int X0 = int.Parse(inputs[0]);
+            int Y0 = int.Parse(inputs[1]);
 
-            int[,][] board = new int[H, W][];
             int[] currentPosition = new int[2];
-            int[] previusPosition = new int[2];
+            int[] prevUpPos = new int[2];
+            int[] prevDownPos = new int[2];
+            int[] prevLeftPos = new int[2];
+            int[] prevRightPos = new int[2];
 
-            currentPosition[0] = X0;
-            currentPosition[1] = Y0;
 
-            for (int h = 0; h < H; H++)
-            {
-                for (int w = 0; w < W; w++)
-                {
-                    board[h, w] = new int[2];
-                    board[h, w][0] = h;
-                    board[h, w][1] = w;
-                }
-            }
+            prevUpPos[0] = prevDownPos[0] = currentPosition[0] = X0;
+            prevUpPos[1] = 0;
+            prevDownPos[1] = H;
+            prevLeftPos[1] = prevRightPos[1] = currentPosition[1] = Y0;
+            prevLeftPos[0] = 0;
+            prevRightPos[0] = W;
 
+            string result = "0 0";
 
             // game loop
             while (true)
             {
                 string bombDir = Console.ReadLine(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
-                int height = 0;
-                int width = 0;
+                Console.Error.WriteLine(bombDir);
+                int height = H;
+                int width = W;
 
-                string result = "0 0";
                 switch (bombDir)
                 {
                     case "U":
-                        height = Math.Round(currentPosition[0] / 2) as decimal;
+                        float coord = currentPosition[1] - (SpecialCase(currentPosition[1], prevUpPos[1]) / 2);
+                        height = (int)Math.Round(coord);
+                        width = currentPosition[0];
+                        prevDownPos[1] = currentPosition[1];
+                        prevDownPos[0] = currentPosition[0];
+                        prevLeftPos[0] = currentPosition[0];
+                        prevLeftPos[1] = currentPosition[1];
+                        prevRightPos[0] = currentPosition[0];
+                        prevRightPos[1] = currentPosition[1];
                         break;
                     case "UR":
+                        coord = currentPosition[1] - (SpecialCase(currentPosition[1], prevUpPos[1]) / 2);
+                        height = (int)Math.Round(coord);
+                        coord = currentPosition[0] + (SpecialCase(prevRightPos[0], currentPosition[0]) / 2);
+                        width = (int)Math.Round(coord);
+                        prevDownPos[1] = currentPosition[1];
+                        prevDownPos[0] = currentPosition[0];
+                        prevLeftPos[0] = currentPosition[0];
+                        prevLeftPos[1] = currentPosition[1];
                         break;
                     case "R":
+                        coord = currentPosition[0] + (SpecialCase(prevRightPos[0], currentPosition[0]) / 2);
+                        height = currentPosition[1];
+                        width = (int)Math.Round(coord);
+                        prevLeftPos[0] = currentPosition[0];
+                        prevLeftPos[1] = currentPosition[1];
+                        prevDownPos[1] = currentPosition[1];
+                        prevDownPos[0] = currentPosition[0];
+                        prevUpPos[1] = currentPosition[1];
+                        prevUpPos[0] = currentPosition[0];
                         break;
                     case "DR":
+                        coord = currentPosition[1] + (SpecialCase(prevDownPos[1], currentPosition[1]) / 2);
+                        height = (int)Math.Round(coord);
+                        coord = currentPosition[0] + (SpecialCase(prevRightPos[0], currentPosition[0]) / 2);
+                        width = (int)Math.Round(coord);
+                        prevUpPos[1] = currentPosition[1];
+                        prevUpPos[0] = currentPosition[0];
+                        prevLeftPos[0] = currentPosition[0];
+                        prevLeftPos[1] = currentPosition[1];
                         break;
                     case "D":
+                        coord = currentPosition[1] + (SpecialCase(prevDownPos[1], currentPosition[1]) / 2);
+                        height = (int)Math.Round(coord);
+                        width = currentPosition[0];
+                        prevUpPos[1] = currentPosition[1];
+                        prevUpPos[0] = currentPosition[0];
+                        prevLeftPos[0] = currentPosition[0];
+                        prevLeftPos[1] = currentPosition[1];
+                        prevRightPos[0] = currentPosition[0];
+                        prevRightPos[1] = currentPosition[1];
                         break;
                     case "DL":
+                        coord = currentPosition[1] + (SpecialCase(prevDownPos[1], currentPosition[1]) / 2);
+                        height = (int)Math.Round(coord);
+                        coord = currentPosition[0] - (SpecialCase(currentPosition[0], prevLeftPos[0]) / 2);
+                        width = (int)Math.Round(coord);
+                        prevUpPos[1] = currentPosition[1];
+                        prevUpPos[0] = currentPosition[0];
+                        prevRightPos[0] = currentPosition[0];
+                        prevRightPos[1] = currentPosition[1];
                         break;
                     case "L":
+                        coord = currentPosition[0] - (SpecialCase(currentPosition[0], prevLeftPos[0]) / 2);
+                        height = currentPosition[1];
+                        width = (int)Math.Round(coord);
+                        prevRightPos[0] = currentPosition[0];
+                        prevRightPos[1] = currentPosition[1];
+                        prevDownPos[1] = currentPosition[1];
+                        prevDownPos[0] = currentPosition[0];
+                        prevUpPos[1] = currentPosition[1];
+                        prevUpPos[0] = currentPosition[0];
                         break;
                     case "UL":
+                        coord = currentPosition[1] - (SpecialCase(currentPosition[1], prevUpPos[1]) / 2);
+                        height = (int)Math.Floor(coord);
+                        coord = currentPosition[0] - (SpecialCase(currentPosition[0], prevLeftPos[0]) / 2);
+                        width = (int)Math.Round(coord);
+                        prevRightPos[0] = currentPosition[0];
+                        prevRightPos[1] = currentPosition[1];
+                        prevDownPos[0] = currentPosition[0];
+                        prevDownPos[1] = currentPosition[1];
                         break;
                 }
 
 
-                previusPosition[0] = currentPosition[0];
-                previusPosition[1] = currentPosition[1];
+                result = width + " " + height;
+                currentPosition[0] = width;
+                currentPosition[1] = height;
 
                 // Write an action using Console.WriteLine()
                 // To debug: Console.Error.WriteLine("Debug messages...");
@@ -84,8 +151,12 @@ namespace CodeInGame
             }
         }
 
-        public static string GetCoordinates(int H, int W){
-            return null;
+        static int SpecialCase(int x, int y)
+        {
+            if ((x - y) == 0)
+                return 2;
+            else
+                return (x - y);
         }
     }
 }
