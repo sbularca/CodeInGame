31b3dbf8d843e0b2222a9b54866b17d0f66e9ae5
uzihead <sebastian@tocaboca.com>
2020-08-02 11:37:14 +0200

    added aneo solution

---
+++ aneo.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int speedLimit = int.Parse(Console.ReadLine());
+        int lightCount = int.Parse(Console.ReadLine());
+        
+        bool hasPassed = false;
+        Dictionary <int, int> distanceWithDuration = new Dictionary<int, int>();
+
+        for (int i = 0; i < lightCount; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int distance = int.Parse(inputs[0]);
+            int duration = int.Parse(inputs[1]);
+
+            distanceWithDuration.Add(distance, duration);
+        }
+
+        while (speedLimit > 0 && !hasPassed)
+        {
+            foreach (var trafficLight in distanceWithDuration)
+            {
+                float result = ((((float)trafficLight.Key / 1000) / (float)speedLimit) * 3600) / (float)(trafficLight.Value);
+
+                //increasing the error margin to make sure it doesn't fail on large data entries
+                if (Math.Truncate((float)Math.Round(result * 100f) / 100f) % 2 != 0)
+                {
+                    hasPassed = false;
+                    break;
+                }
+                else 
+                {
+                    hasPassed = true;
+                }
+            }
+
+            if (hasPassed)
+            {
+                break;
+            }
+            else
+            {
+                speedLimit--;
+            }
+        }
+
+        Console.WriteLine(speedLimit);
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+    }
+}
