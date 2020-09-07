67179b7b39f853a7de807b8071ec943076edb78c
uzihead <sebastian@tocaboca.com>
2020-09-07 16:48:40 +0200

    added mars lander

---
+++ MarsLanderTwo.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class MarsLanderTwo
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        int surfaceN = int.Parse(Console.ReadLine()); // the number of points used to draw the surface of Mars.
+        float maxSpeed = 40;
+        
+        for (int i = 0; i < surfaceN; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int landX = int.Parse(inputs[0]); // X coordinate of a surface point. (0 to 6999)
+            int landY = int.Parse(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        }
+
+        // game loop
+        while (true)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int X = int.Parse(inputs[0]);
+            int Y = int.Parse(inputs[1]);
+            int hSpeed = int.Parse(inputs[2]); // the horizontal speed (in m/s), can be negative.
+            int vSpeed = int.Parse(inputs[3]); // the vertical speed (in m/s), can be negative.
+            int fuel = int.Parse(inputs[4]); // the quantity of remaining fuel in liters.
+            int rotate = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
+            int power = int.Parse(inputs[6]); // the thrust power (0 to 4).
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+            
+            vSpeed = Math.Abs(vSpeed);
+
+            float time = vSpeed>0 ? Y/vSpeed : time=0;
+            float idealTime = Y/maxSpeed;
+
+            if (time < idealTime) power++;
+            if (time > idealTime) power--;
+
+            if (power > 4) power=4;
+            if (power < 0) power=0;
+
+            // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
+            Console.WriteLine("-20 3");
+        }
+    }
+}
--- WarGame.cs
+++ WarGame.cs
@@ -11,95 +11,95 @@ using System.Collections.Generic;
  **/
 class WarGame
 {
-    private static string [] deck = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-
     static void Main(string[] args)
     {
-        
-        Queue<string> player1Cards = new Queue<string>();
-        Queue<string> player2Cards = new Queue<string>();
-        List<string> onTableP1 = new List<string>();
-        List<string> onTableP2 = new List<string>();
-        
+        string [] deck = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        Queue<string> cardsPlayer1 = new Queue<string>();
+        Queue<string> cardsPlayer2 = new Queue<string>();
+
         int turns = 0;
         
         int n = Int32.Parse(Console.ReadLine()); // the number of cards for player 1
         for (int i = 0; i < n; i++)
         {
             string cardp1 = Console.ReadLine(); // the n cards of player 1
-            player1Cards.Enqueue(cardp1?.Remove(cardp1.Length - 1));
+            cardsPlayer1.Enqueue(cardp1?.Remove(cardp1.Length - 1));
         }
         
         int m = Int32.Parse(Console.ReadLine()); // the number of cards for player 2
         for (int i = 0; i < m; i++)
         {
             string cardp2 = Console.ReadLine(); // the m cards of player 2
-            player2Cards.Enqueue(cardp2?.Remove(cardp2.Length - 1));
+            cardsPlayer2.Enqueue(cardp2?.Remove(cardp2.Length - 1));
         }
         
-        while (player1Cards.Count > 0 && player2Cards.Count > 0)
+        while (cardsPlayer1.Count > 0 && cardsPlayer2.Count > 0)
         {
-            onTableP1.Clear();
-            onTableP2.Clear();
+            List<string> playedPlayer1 = new List<string>();
+            List<string> playedPlayer2 = new List<string>();
             
-            Repeat:    
-            var cp1 = player1Cards.Dequeue(); 
-            var cp2 = player2Cards.Dequeue();
-
-            onTableP1.Add(cp1);
-            onTableP2.Add(cp2);
+            // Goto/repeat is the fastest option
+            Repeat:
+            var cp1 = cardsPlayer1.Dequeue();
+            playedPlayer1.Add(cp1);
+            
+            var cp2 = cardsPlayer2.Dequeue();
+            playedPlayer2.Add(cp2);
             
             if (cp1 == cp2)
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    if (player1Cards.Count <= 0)
+                    if (cardsPlayer1.Count <= 0)
                     {
                         Console.WriteLine("PAT");
                         return;
                     }
-                    onTableP1.Add(player1Cards.Dequeue());
+                    
+                    playedPlayer1.Add(cardsPlayer1.Dequeue());
                 }
                 
                 for (int i = 0; i < 3; i++)
                 {
-                    if (player2Cards.Count <= 0)
+                    if (cardsPlayer2.Count <= 0)
                     {
                         Console.WriteLine("PAT");
                         return;
                     }
-                    onTableP2.Add(player2Cards.Dequeue());
+                    
+                    playedPlayer2.Add(cardsPlayer2.Dequeue());
                 }
                 goto Repeat;
             }
-
-            onTableP1.AddRange(onTableP2);
+            
+            // Add all cards in one list for easy enqueuing
+            playedPlayer1.AddRange(playedPlayer2);
             
             if (Array.IndexOf(deck, cp1) > Array.IndexOf(deck, cp2))
             {
-                foreach (var card in onTableP1)
+                foreach (var card in playedPlayer1)
                 {
-                    player1Cards.Enqueue(card);
+                    cardsPlayer1.Enqueue(card);
                 }
             }
             else
             {
-                foreach (var card in onTableP1)
+                foreach (var card in playedPlayer1)
                 {
-                    player2Cards.Enqueue(card);
+                    cardsPlayer2.Enqueue(card);
                 }
             }
             
             turns++;
         }
 
-        if (player1Cards.Count == 0)
+        if (cardsPlayer1.Count == 0)
         {
             Console.WriteLine($"2 {turns}");
             return;
         }
 
-        if (player2Cards.Count == 0)
+        if (cardsPlayer2.Count == 0)
         {
             Console.WriteLine($"1 {turns}");
         }
