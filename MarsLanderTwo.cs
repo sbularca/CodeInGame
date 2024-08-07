b76f0a1fa1f21c3b7bf61afc3052aa5db34b3b7c
Sebastian Bularca <sebastian.bularca@resolutiongames.com>
2024-08-07 10:37:40 +0200

    added chat gpt code for mars lander

--- MarsLanderTwo.cs
+++ MarsLanderTwo.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
 
 /**
@@ -13,7 +10,6 @@ class MarsLanderTwo {
     public static void Execute() {
         string[] inputs;
         int surfaceN = int.Parse(Console.ReadLine()); // the number of points used to draw the surface of Mars.
-        float maxSpeed = 40;
 
         for(int i = 0; i < surfaceN; i++) {
             inputs = Console.ReadLine().Split(' ');
@@ -32,22 +28,135 @@ class MarsLanderTwo {
             int rotate = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
             int power = int.Parse(inputs[6]); // the thrust power (0 to 4).
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-
-            vSpeed = Math.Abs(vSpeed);
-
-            float time = vSpeed > 0 ? Y / vSpeed : time = 0;
-            float idealTime = Y / maxSpeed;
-
-            if(time < idealTime) power++;
-            if(time > idealTime) power--;
-
-            if(power > 4) power = 4;
-            if(power < 0) power = 0;
-
             // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
             Console.WriteLine("-20 3");
+        }
+    }
+
+    class Program {
+        public static Random random = new Random();
+        public const int PopulationSize = 100;
+        public const int GeneLength = 100;  // Number of commands in a solution
+        public const int MaxGenerations = 1000;
+        public const double MutationRate = 0.01;
+        public const double Gravity = 3.711;
+
+        static void Main(string[] args) {
+            // Initial state: x, y, hSpeed, vSpeed, fuel, rotation, power
+            var initialState = new State(2500, 2500, 0, 0, 550, 0, 0);
+
+            var population = new List<Solution>();
+            for(int i = 0; i < PopulationSize; i++) {
+                population.Add(new Solution(GeneLength));
+            }
+
+            for(int generation = 0; generation < MaxGenerations; generation++) {
+                foreach(var sol in population) {
+                    sol.Evaluate(initialState);
+                }
+
+                population = population.OrderByDescending(sol => sol.Fitness).ToList();
+                var parents = population.Take(PopulationSize / 2).ToList();
+
+                var newPopulation = new List<Solution>();
+                for(int i = 0; i < PopulationSize; i++) {
+                    var parent1 = parents[random.Next(parents.Count)];
+                    var parent2 = parents[random.Next(parents.Count)];
+                    var child = parent1.Crossover(parent2);
+                    child.Mutate();
+                    newPopulation.Add(child);
+                }
+
+                population = newPopulation;
+
+                Console.WriteLine($"Generation {generation}: Best fitness = {population[0].Fitness}");
+            }
+
+            var bestSolution = population[0];
+            foreach(var gene in bestSolution.Genes) {
+                Console.WriteLine($"{gene.Rotation} {gene.Thrust}");
+            }
+        }
+    }
+
+    class State {
+        public double X, Y, HSpeed, VSpeed, Fuel, Rotation, Power;
+
+        public State(double x, double y, double hSpeed, double vSpeed, double fuel, double rotation, double power) {
+            X = x;
+            Y = y;
+            HSpeed = hSpeed;
+            VSpeed = vSpeed;
+            Fuel = fuel;
+            Rotation = rotation;
+            Power = power;
+        }
+
+        public State Clone() {
+            return new State(X, Y, HSpeed, VSpeed, Fuel, Rotation, Power);
+        }
+    }
+
+    class Gene {
+        public int Rotation, Thrust;
+        public static Random random = new Random();
+
+        public Gene(int rotation, int thrust) {
+            Rotation = rotation;
+            Thrust = thrust;
+        }
+
+        public static Gene RandomGene() {
+            return new Gene(random.Next(-15, 16), random.Next(0, 5));
+        }
+    }
+
+    class Solution {
+        public List<Gene> Genes;
+        public double Fitness;
+        public static Random random = new Random();
+
+        public Solution(int geneLength) {
+            Genes = new List<Gene>();
+            for(int i = 0; i < geneLength; i++) {
+                Genes.Add(Gene.RandomGene());
+            }
+        }
+
+        public void Evaluate(State initialState) {
+            var state = initialState.Clone();
+            foreach(var gene in Genes) {
+                state.Rotation = Math.Max(-90, Math.Min(90, state.Rotation + gene.Rotation));
+                state.Power = Math.Max(0, Math.Min(4, state.Power + gene.Thrust));
+                state.Fuel -= state.Power;
+                var rad = state.Rotation * (Math.PI / 180);
+                state.HSpeed += -Math.Sin(rad) * state.Power;
+                state.VSpeed += Math.Cos(rad) * state.Power - Program.Gravity;
+                state.X += state.HSpeed;
+                state.Y += state.VSpeed;
+                if(state.Y <= 0) {
+                    break;
+                }
+            }
+
+            // Fitness: minimize horizontal and vertical speed at landing, stay within landing zone
+            Fitness = Math.Max(0, 1000 - (Math.Abs(state.HSpeed) + Math.Abs(state.VSpeed) + Math.Abs(state.X - 2500) + Math.Abs(state.Y)));
+        }
+
+        public Solution Crossover(Solution other) {
+            var child = new Solution(Genes.Count);
+            for(int i = 0; i < Genes.Count; i++) {
+                child.Genes[i] = (random.NextDouble() < 0.5) ? Genes[i] : other.Genes[i];
+            }
+            return child;
+        }
+
+        public void Mutate() {
+            for(int i = 0; i < Genes.Count; i++) {
+                if(Program.random.NextDouble() < Program.MutationRate) {
+                    Genes[i] = Gene.RandomGene();
+                }
+            }
         }
     }
 }
