61dcf5aec98ad7f6dbb7a70831938e7380b0fb83
Sebastian Bularca <sebastian.bularca@resolutiongames.com>
2024-08-09 16:14:23 +0200

    added more useful code

--- MarsLanderTwo.cs
+++ MarsLanderTwo.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.IO;
+using System.Dynamic;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -8,10 +10,22 @@ using System.Collections.Generic;
  **/
 class MarsLanderTwo {
     static int landingZoneLeft, landingZoneRight, landingZoneY;
+    static int highestPointX, highestPointY;
+    static string testData;
 
-    static void Main(string[] args) {
+
+    public static void Execute() {
+        // Redirect standard input to read from a string (for testing purposes)
+        testData = @"7
+0 100
+1000 500
+2000 1500
+3000 1000
+4000 1000
+5000 1600
+6000 500";
+        Console.SetIn(new StringReader(testData));
         ReadSurfaceData();
-
         while(true) {
             var (x, y, hSpeed, vSpeed, fuel, rotate, power) = ReadLanderState();
             var (newRotate, newPower) = CalculateNextMove(x, y, hSpeed, vSpeed);
@@ -21,45 +35,92 @@ class MarsLanderTwo {
 
     static void ReadSurfaceData() {
         int surfaceN = int.Parse(Console.ReadLine());
-        int prevX = -1, prevY = -1;
+        highestPointY = int.MinValue;
+
+        List<(int x, int y)> surfacePoints = new List<(int x, int y)>();
+
         for(int i = 0; i < surfaceN; i++) {
             var inputs = Console.ReadLine().Split(' ');
-            int landX = int.Parse(inputs[0]);
-            int landY = int.Parse(inputs[1]);
-            if(landY == prevY) {
-                landingZoneLeft = prevX;
-                landingZoneRight = landX;
-                landingZoneY = landY;
-            }
-            prevX = landX;
-            prevY = landY;
+            surfacePoints.Add((int.Parse(inputs[0]), int.Parse(inputs[1])));
         }
+        (landingZoneLeft, landingZoneRight, landingZoneY) = FindLandingZone(surfacePoints);
+
+        (highestPointX, highestPointY) = FindHighestCoordinateBetweenShipAndLandingZone(surfacePoints, landingZoneLeft, landingZoneY);
+
+        Console.WriteLine($"Highest Point: ({highestPointX}, {highestPointY})");
     }
 
-    static (int x, int y, int hSpeed, int vSpeed, int fuel, int rotate, int power) ReadLanderState() {
+    static (int left, int right, int y) FindLandingZone(List<(int x, int y)> surfacePoints) {
+        for(int i = 0; i < surfacePoints.Count - 1; i++) {
+            if(surfacePoints[i].y == surfacePoints[i + 1].y) {
+                landingZoneLeft = surfacePoints[i].x;
+                landingZoneRight = surfacePoints[i + 1].x;
+                landingZoneY = surfacePoints[i].y;
+            }
+        }
+        Console.WriteLine($"Landing Zone: ({landingZoneLeft}, {landingZoneRight}, {landingZoneY})");
+        return (landingZoneLeft, landingZoneRight, landingZoneY);
+    }
+
+    static (int x, int y) FindHighestCoordinateBetweenShipAndLandingZone(List<(int x, int y)> surfacePoints, int landingZoneLeft, int landingZoneRight) {
+        return surfacePoints.Where(point => point.x < landingZoneLeft).OrderByDescending(point => point.y).First();
+    }
+
+    static (int coordX, int coorY, int hSpeed, int vSpeed, int fuel, int rotate, int power) ReadLanderState() {
         var inputs = Console.ReadLine().Split(' ');
         return (
             int.Parse(inputs[0]), int.Parse(inputs[1]),
             int.Parse(inputs[2]), int.Parse(inputs[3]),
             int.Parse(inputs[4]), int.Parse(inputs[5]), int.Parse(inputs[6])
         );
+
     }
 
     static (int rotate, int power) CalculateNextMove(int x, int y, int hSpeed, int vSpeed) {
-        int targetX = landingZoneLeft + (landingZoneRight - landingZoneLeft) / 10;
+        int targetX = landingZoneLeft + (landingZoneRight - landingZoneRight) / 10;
         int rotate = 0;
         int power = 3;
 
-        if(Math.Abs(x - targetX) > 0) {
-            rotate = x < targetX ? -30 : 30;
+        if(x < targetX - 100) {
+            rotate = -15;
         }
-
-        if(y - landingZoneY < 1000 && Math.Abs(hSpeed) < 20) {
+        else if(x > targetX + 100) {
+            rotate = 15;
+        }
+        else {
             rotate = 0;
         }
 
-        if(vSpeed > -30) {
+        if(vSpeed < -30) {
             power = 4;
+        }
+        else if(vSpeed > -10) {
+            power = 2;
+        }
+        else {
+            power = 3;
+        }
+
+
+        if(y < highestPointY + 100) {
+            power = 4;
+        }
+
+        if(x < targetX) {
+            rotate = -15;
+        }
+        else if(x > targetX) {
+            rotate = 15;
+        }
+
+        if(vSpeed < -30) {
+            power = 4;
+        }
+        else if(vSpeed > -10) {
+            power = 2;
+        }
+        else {
+            power = 3;
         }
 
         return (rotate, power);
