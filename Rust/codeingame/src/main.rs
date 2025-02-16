81ce09e58dff9d960e337a0221916ff8f5737ffb
sbularca <sebastian.bularca@resolutiongames.com>
2025-02-16 22:23:02 +0100

    added island_escape

--- Rust/codeingame/src/main.rs
+++ Rust/codeingame/src/main.rs
@@ -1,6 +1,8 @@
 mod six_degrees;
+mod island_escape;
 
 fn main() {
     println!("Hello, world!");
-    six_degrees::six_degrees();
+    //six_degrees::six_degrees();
+    island_escape::island_escape();
 }
--- Rust/codeingame/src/six_degrees.rs
+++ Rust/codeingame/src/six_degrees.rs
@@ -12,10 +12,14 @@ macro_rules! parse_input {
 pub fn six_degrees() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
+
     let actor_name = input_line.trim_matches('\n').to_string();
     let mut input_line = String::new();
+
     io::stdin().read_line(&mut input_line).unwrap();
+
     let n = parse_input!(input_line, i32);
+
     for i in 0..n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
